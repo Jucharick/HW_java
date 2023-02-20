a739ac04d29a4bf48588e13b1a9e8bacd9ecfc41
Julia Ch <juliecha@yandex.ru>
2023-02-21 00:01:00 +0300

    UPD Task_001 - sortMerge

--- Lesson_3/Task_001.java
+++ Lesson_3/Task_001.java
@@ -6,55 +6,61 @@ import java.util.Arrays;
 
 public class Task_001 {
     public static void main(String[] args) {
-        int[] arrayNum = {1, 4, 5, 3, 9, 7, 2, 7, 9};
+        int[] arrayNum = {1, 4, 5, 3, 9, 7, 2, 7, 9, -5, 123, 44};
 
         System.out.println(Arrays.toString(arrayNum));
-        System.out.println(Arrays.toString(sortMerge(arrayNum)));
+        sortMerge(arrayNum);
+        System.out.println(Arrays.toString(arrayNum));
     }
 
-    private static int[] sortMerge(int[] array) {
-        int[] temp;
-        int[] currentSrc = array; // массив - источник, откуда мы сливаем массивы друг с другом
-        int[] currentDest = new int[array.length]; // массив - приеник, куда мы заливаем результирующие массивы после слияния
+    private static void sortMerge(int[] arraySort) {
+        int size = arraySort.length;
 
-        int size = 1;
-        while (size < array.length) {
-            for (int i = 0; i < array.length; i += 2 * size) {
-                merge(currentSrc, i, currentSrc, i + size, currentDest, i, size);
-            }
-            temp = currentSrc;
-            currentSrc = currentDest;
-            currentDest = temp;
-            size = size * 2;
+        int middle = size / 2;
+        if (size == 1) return;
+
+        int [] left = new int[middle];
+        int [] right = new int[size - middle];
+
+        for (int i = 0; i < left.length; i++) {
+            left[i] = arraySort[i];
         }
-        return currentSrc;
+        for (int i = middle; i < size; i++) {
+            right[i - middle] = arraySort[i];
+        }
+
+        sortMerge(left);
+        sortMerge(right);
+        merge(arraySort, left, right);
     }
 
-    private static void merge(int[] left, int leftStart, int[] right, int rightStart, int[] dest,
-                              int destStart, int size) {
-        int indexL = leftStart;
-        int indexR = rightStart;
+    private static void merge(int [] arraySort, int [] left, int [] right) {
+        int lengthLeft = left.length;
+        int lengthRight = right.length;
+        int indexLeft = 0;
+        int indexRight = 0;
+        int idx = 0;
 
-        int leftEnd = Math.min(leftStart + size, left.length);
-        int right2End = Math.min(rightStart + size, right.length);
-
-        if (leftStart + size > left.length) {
-            for (int i = leftStart; i < leftEnd; i++) {
-                dest[i] = left[i];
-            }
-            return;
-        }
-
-        int iterationCount = leftEnd - leftStart + right2End - rightStart; // сколько итераций цикла для объединения двух массивов - суммарное количество элементов в двух подмассивах
-
-        for (int i = destStart; i < destStart + iterationCount; i++) {
-            if (indexL < leftEnd && (indexR >= right2End || left[indexL] < right[indexR])) { // indexL < leftEnd && (indexR >= right2End - когда в первом или во втором массиве заканчиваются элементы
-                dest[i] = left[indexL];
-                indexL++;
+        while (indexLeft < lengthLeft && indexRight < lengthRight) {
+            if (left[indexLeft] < right[indexRight]){
+                arraySort[idx] = left[indexLeft];
+                indexLeft++;
+                idx++;
             } else {
-                dest[i] = right[indexR];
-                indexR++;
+                arraySort[idx] = right[indexRight];
+                indexRight++;
+                idx++;
             }
+        }
+
+        for (int ll = indexLeft; ll < lengthLeft; ll++) {
+            arraySort[idx] = left[ll];
+            idx++;
+        }
+
+        for (int rr = indexRight; rr < lengthRight; rr++) {
+            arraySort[idx] = right[rr];
+            idx++;
         }
     }
 }
