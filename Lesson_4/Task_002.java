e8d4a160b799049969d5c6888705b5da6c42ff6a
Julia Ch <juliecha@yandex.ru>
2023-02-27 23:28:52 +0300

    Add Task_002.java - LinkedList

---
+++ Lesson_4/Task_002.java
@@ -0,0 +1,48 @@
+// Реализуйте очередь с помощью LinkedList со следующими методами:
+// enqueue() - помещает элемент в конец очереди, dequeue() - возвращает первый элемент из очереди и удаляет его, first() - возвращает первый элемент из очереди, не удаляя.
+
+package Lesson_4;
+
+import java.util.LinkedList;
+
+public class Task_002 {
+    private static LinkedList<Integer> list = new LinkedList<Integer>();
+
+    public static void main(String[] args) {
+        enqueue(1); // enqueue() - помещает элемент в конец очереди
+        enqueue(2);
+        enqueue(3);
+        enqueue(12);
+        enqueue(23);
+        enqueue(4);
+
+        System.out.println(size());
+        System.out.println(isEmpty(0));
+        System.out.println(first()); // first() - возвращает первый элемент из очереди, не удаляя
+
+        System.out.println(dequeue()); // dequeue() - возвращает первый элемент из очереди и удаляет его
+        System.out.println(dequeue());
+        System.out.println(dequeue());
+        System.out.println(list);
+    }
+
+    public static void enqueue (int element){
+        list.add(element);
+    }
+
+    public static int dequeue (){
+        return list.removeFirst();
+    }
+
+    public static int first (){
+        return list.getFirst();
+    }
+
+    public static int size (){
+        return list.size();
+    }
+
+    public static boolean isEmpty (int element){
+        return list.isEmpty();
+    }
+}
