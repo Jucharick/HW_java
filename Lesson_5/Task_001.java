9e817df6957ed4b9f0a1f4588633cb784a3a3189
Julia Ch <juliecha@yandex.ru>
2023-03-02 22:53:12 +0300

    UPD Task_001 with Class PhoneWithLabel

--- Lesson_5/Task_001.java
+++ Lesson_5/Task_001.java
@@ -11,25 +11,25 @@ public class Task_001 {
     public static void main(String[] args) {
         Map<String, List<PhoneWithLabel>> namesToPhoneWithLabel = new HashMap<>();
         
-        List<PhoneWithLabel> withLabels1 = new ArrayList<>();
-        withLabels1.add(new PhoneWithLabel("mobile", 1324353450));
-        withLabels1.add(new PhoneWithLabel("work", 1234567890));
-        withLabels1.add(new PhoneWithLabel("mobile", 1098765432));
-        withLabels1.add(new PhoneWithLabel("home", 1098765432));
+        List<PhoneWithLabel> ivansPhone = new ArrayList<>();
+        ivansPhone.add(new PhoneWithLabel("mobile", 1324353450));
+        ivansPhone.add(new PhoneWithLabel("work", 1234567890));
+        ivansPhone.add(new PhoneWithLabel("mobile", 1098765432));
+        ivansPhone.add(new PhoneWithLabel("home", 1098765432));
 
-        namesToPhoneWithLabel.put("Ivan Ivanov", withLabels1);
+        namesToPhoneWithLabel.put("Ivan Ivanov", ivansPhone);
 
-        List<PhoneWithLabel> withLabels2 = new ArrayList<>();
-        withLabels2.add(new PhoneWithLabel("mobile", 1324353450));
-        withLabels2.add(new PhoneWithLabel("work", 1234567890));
+        List<PhoneWithLabel> petersPhone = new ArrayList<>();
+        petersPhone.add(new PhoneWithLabel("mobile", 1324353450));
+        petersPhone.add(new PhoneWithLabel("work", 1234567890));
 
-        namesToPhoneWithLabel.put("Petr Petrov", withLabels2);
+        namesToPhoneWithLabel.put("Petr Petrov", petersPhone);
 
-        List<PhoneWithLabel> withLabels3 = new ArrayList<>();
-        withLabels3.add(new PhoneWithLabel("mobile", 1098765432));
-        withLabels3.add(new PhoneWithLabel("home", 1098765432));
+        List<PhoneWithLabel> sidorsPhone = new ArrayList<>();
+        sidorsPhone.add(new PhoneWithLabel("mobile", 1098765432));
+        sidorsPhone.add(new PhoneWithLabel("home", 1098765432));
 
-        namesToPhoneWithLabel.put("Sidr Sidorov", withLabels3);
+        namesToPhoneWithLabel.put("Sidr Sidorov", sidorsPhone);
 
 
         System.out.println("Book: ");
